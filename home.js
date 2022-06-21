179b25120a97e8d9d7fb66cd1a324bfd1c90b010
t1an <107872003+t1anxxx@users.noreply.github.com>
2022-06-21 14:44:46 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+$('.btn-hide').click(function(){
+    $('p').slideUp();
+});
+$('.btn-show').click(function(){
+    $('p').slideDown();
+});
+$('.btn-toggle').click(function(){
+    $('p').slideToggle();
+});
